5e813ca98bc11b3dc2cf53278d78df6407e6f991
jacksplwxy <895685537@qq.com>
2019-04-15 23:45:48 +0800

    装饰器在java中的实现

---
+++ src/Decorator（装饰器）/common.java
@@ -0,0 +1,65 @@
+//抽象构件角色
+interface  Component
+{
+    public void operation();
+}
+//具体构件角色
+class ConcreteComponent implements Component
+{
+    public ConcreteComponent()
+    {
+        System.out.println("创建具体构件角色");       
+    }   
+    public void operation()
+    {
+        System.out.println("调用具体构件角色的方法operation()");           
+    }
+}
+//抽象装饰角色
+class Decorator implements Component
+{
+    private Component component;   
+    public Decorator(Component component)
+    {
+        this.component=component;
+    }   
+    public void operation()
+    {
+        component.operation();
+    }
+}
+//具体装饰角色
+class ConcreteDecorator extends Decorator
+{
+    public ConcreteDecorator(Component component)
+    {
+        super(component);
+    }   
+    public void operation()
+    {
+        super.operation();
+        addedFunction();
+    }
+    public void addedFunction()
+    {
+        System.out.println("为具体构件角色增加额外的功能addedFunction()");           
+    }
+}
+
+public class Client
+{
+    public static void main(String[] args)
+    {
+        Component p=new ConcreteComponent();
+        p.operation();
+        System.out.println("---------------------------------");
+        Component d=new ConcreteDecorator(p);
+        d.operation();
+    }
+}
+
+创建具体构件角色
+调用具体构件角色的方法operation()
+---------------------------------
+调用具体构件角色的方法operation()
+为具体构件角色增加额外的功能addedFunction()
