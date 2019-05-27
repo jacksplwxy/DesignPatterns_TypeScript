c03af1d3b7b861734d1b8a0936989c5ac311a1e0
jacksplwxy <895685537@qq.com>
2019-05-27 09:33:59 +0800

    Update common.ts

--- src/Mediator（中介者）/common.ts
+++ src/Mediator（中介者）/common.ts
@@ -3,25 +3,22 @@
 /**
  * 中介者：用于模块之间通信。
  */
-
 //抽象中介者
 abstract class Mediator {
-    public abstract register(colleague: Colleague): void;
-    public abstract relay(cl: Colleague): void; //转发
-}
-//具体中介者
-class ConcreteMediator extends Mediator {
-    private colleagues: Array<Colleague> = []
+    //存储
+    public colleagues: Array<Colleague> = []
+    //注册
     public register(colleague: Colleague): void {
         if (this.colleagues.indexOf(colleague) === -1) {
             this.colleagues.push(colleague)
             colleague.setMedium(this)
         }
     }
-    public relay(cl: Colleague): void {
+    //转发
+    public relay(cl: Colleague, message?: any): void {
         this.colleagues.forEach((colleague: Colleague, index) => {
             if (cl !== colleague) {
-                colleague.receive()
+                colleague.receive(message)
             }
         })
     }
@@ -32,50 +29,63 @@ abstract class Colleague {
     public setMedium(mediator: Mediator): void {
         this.mediator = mediator;
     }
-    public abstract receive(): void;
-    public abstract send(): void;
+    public receive(message?: any): void {
+        console.log(this.constructor.name + "收到请求：", message);
+    }
+    public send(message?: any): void {
+        console.log(this.constructor.name + "发出请求:", message);
+        this.mediator.relay(this, message); //请中介者转发
+    }
 }
-//具体同事类
+
+
+//具体中介者
+class ConcreteMediator extends Mediator {
+    constructor() {
+        super()
+    }
+}
+//具体同事类1
 class ConcreteColleague1 extends Colleague {
     constructor() {
         super()
     }
-    public receive(): void {
-        console.log("具体同事类1收到请求。");
-    }
-    public send(): void {
-        console.log("具体同事类1发出请求。");
-        this.mediator.relay(this); //请中介者转发
+    public receive(message?: any): void {
+        if (message === '大家嗨起来') {
+            console.log(this.constructor.name + '：drink、drink、drink')
+        } else if (message === '大家静下来') {
+            console.log(this.constructor.name + '：no、no、no')
+        }
     }
 }
-//具体同事类
+//具体同事类2
 class ConcreteColleague2 extends Colleague {
     constructor() {
         super()
     }
-    public receive(): void {
-        console.log("具体同事类2收到请求。");
-    }
-    public send(): void {
-        console.log("具体同事类2发出请求。");
-        this.mediator.relay(this); //请中介者转发
+    public receive(message?: any): void {
+        if (message === '大家嗨起来') {
+            console.log(this.constructor.name + '：dance、dance、dance')
+        } else if (message === '大家静下来') {
+            console.log(this.constructor.name + '：no、no、no')
+        }
     }
 }
-//具体同事类
+//具体同事类3
 class ConcreteColleague3 extends Colleague {
     constructor() {
         super()
     }
-    public receive(): void {
-        console.log("具体同事类3收到请求。");
-    }
-    public send(): void {
-        console.log("具体同事类3发出请求。");
-        this.mediator.relay(this); //请中介者转发
+    public receive(message?: any): void {
+        if (message === '大家嗨起来') {
+            console.log(this.constructor.name + '：drink、drink、drink')
+        } else if (message === '大家静下来') {
+            console.log(this.constructor.name + '：no、no、no')
+        }
     }
 }
 
-class ConcreteMediatorClient {
+class Client {
     public static main(): void {
         let md: Mediator = new ConcreteMediator();
         let c1: Colleague = new ConcreteColleague1();
@@ -84,7 +94,15 @@ class ConcreteMediatorClient {
         md.register(c1);
         md.register(c2);
         md.register(c3);
-        c1.send();
-        c2.send();
+        c1.send('大家嗨起来');
+        c2.send('大家静下来');
     }
 }
+Client.main()
+
+// ConcreteColleague1发出请求: 大家嗨起来
+// ConcreteColleague2：dance、dance、dance
+// ConcreteColleague3：drink、drink、drink
+// ConcreteColleague2发出请求: 大家静下来
+// ConcreteColleague1：no、no、no
+// ConcreteColleague3：no、no、no
